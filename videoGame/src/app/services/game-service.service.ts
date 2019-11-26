dc0fd2f23eecd94b67adc4d33b023938d1231dcd
Michael Whelan <whelano07@hotmail.com>
2019-11-26 20:24:04 +0000

    added read and services

---
+++ videoGame/src/app/services/game-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameServiceService {
+
+  constructor(private httpc: HttpClient) { }
+
+getgames(): Observable <any>{
+  return this.httpc.get('https://jsonblob.com/9417cbbe-108a-11ea-827d-a3afa4296f98')
+  }
+}
