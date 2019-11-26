02ff1b5e664e0cb2fa080690ae8991618eff53af
Michael Whelan <whelano07@hotmail.com>
2019-11-26 20:29:58 +0000

    added json

--- videoGame/src/app/read/read.component.ts
+++ videoGame/src/app/read/read.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GameServiceService} from '../services/game-service.service'
 
 @Component({
   selector: 'app-read',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadComponent implements OnInit {
 
-  constructor() { }
+  constructor( private gameService: GameServiceService) { }
+  myGames: any;
 
   ngOnInit() {
-  }
-
-}
+    this.gameService.getgames().subscribe((data) => {
+      this.myGames = data.Search;
+      });
+      }
+      }
