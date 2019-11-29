0671ebcfc645ea92f253774040c5df21d7ae6843
Michael Whelan <whelano07@hotmail.com>
2019-11-29 17:21:08 +0000

    fixed issues

--- videoGame/Backend/server.js
+++ videoGame/Backend/server.js
@@ -1,29 +1,114 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
 const cors = require('cors');
-app.use(cors());
-app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
-});
+const mongoose = require('mongoose');
 
-app.get('/api/games', (req, res)=>{
-    const games = [
-    {
+const mongoDB = 'mongodb+srv://root:Mike1991@cluster0-dg5xo.mongodb.net/project?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, {useNewUrlParser:true});
 
-    "Title": "The Witcher 3: Wild Hunt - Blood and Wine",
-    "Year": "2016",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMTg2ZmY0MGUtZmFjZS00YjkxLTlmMWEtMDE0ZWQwYzBlODA2XkEyXkFqcGdeQXVyMzUwNzgzNzg@._V1_UY268_CR13,0,182,268_AL_.jpg"
-    },
-    {
-    "Title": "The Last of Us",
-    "Year": "2013",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZjExN2JiMzAtZGRiNi00M2Q5LWEzYjItYzRkYWE0ZmRhN2E5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SY1000_SX708_AL_.jpg" 
-}]
-;
-res.status(200).json({
-message: "Everything is good",
-myMovies:movies
-})
-})
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+    title:String,
+    year:String,
+    poster:String
+  });
+  
+  const GameModel = mongoose.model('game',gameSchema);
+  
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
+  
+  // respond with "hello world" when a GET request is made to the homepage
+  app.get('/', (req, res) => {
+    res.send('hello world');
+  })
+  
+  app.get('/api/games', (req,res,next) => {
+  
+    console.log("get request")
+    GameModel.find((err,data)=>{
+      res.json({games:data});
+    })
+  })
+  
+  app.delete('/api/games/:id', (req,res) =>{
+    console.log(req.params.id);
+  
+    GameModel.deleteOne({_id:req.params.id},(error,data)=>{
+      if(error)
+        res.json(error);
+        
+      res.json(data);
+    })
+  })
+  
+  app.get('/api/games/:id', (req, res, next) => {
+    console.log(req.params.id);
+    MovieModel.findById(req.params.id,
+      function (err, data) {
+        res.json(data);
+      });
+  })
+  app.put('/api/games/:id', function (req, res) {
+    console.log("Update Game " + req.params.id);
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.year);
+    console.log(req.body.poster);
+    GameModel.findByIdAndUpdate(req.params.id, req.body, { new: true },
+      function (err, data) {
+        res.send(data);
+      })
+  })
+  
+  
+  
+  
+  app.post('/api/games', (req,res) =>{
+  console.log('post Sucessfull');
+  console.log(req.body)
+  console.log(req.body.title);
+  console.log(req.body.year);
+  console.log(req.body.poster);
+  
+  GameModel.create({
+    title: req.body.title,
+    year: req.body.year,
+    poster: req.body.poster
+  });
+  res.json('data uploaded')
+  
+  
+  })
+  
+  app.get('/api/games/:id',(req,res)=>{
+    console.log(req.params.id);
+  
+    GameModel.findById(req.params.id, (err, data)=>{
+      res.json(data);
+    })
+  })
+  
+  app.use("/", express.static(path.join(__dirname, "angular")));
+  app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+  })
+  
+  
+          
+  
+  app.listen(PORT, function () {
+    console.log('Server is running on Port: ', PORT);
+  });
--- videoGame/src/app/services/game-service.service.ts
+++ videoGame/src/app/services/game-service.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Game } from '../game.model';
+import {Game} from '../game.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameServiceService {
 
-  constructor(private httpc: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  GetGameInformation(): Observable <any>{
-    return this.httpc.get('http://localhost:4000/api/movies');
+
+  getGameInformation(): Observable<any> {
+    return this.http.get('http://localhost:4000/api/movies');
   }
 
-  AddGameInformation(title: string,year: string,poster: string): Observable<any>{
-    const game: Game = {title, year, poster};
-    return this.httpc.post('http://localhost:3000/api/movies', game);
-    }
+  AddGameInformation(title: string, year: string, poster: string): Observable<any> {
+    const movie: Game = {title, year, poster};
+    return this.http.post('http://localhost:4000/api/movies', movie); }
+
+        getGame(id: string): Observable<any> {
+return this.http.get('http://localhost:4000/api/movies/' + id);
+}
 }
